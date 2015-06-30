5e08c1ec9746ad9ca4b5d48d7d0be33a4b9c3f7f
salvere0 <apporiate@naver.com>
2015-06-30 15:21:23 +0900

    20150630
    오늘한거

---
+++ 20150630/Assets/CubeRotate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour {
+	//public float speed;
+	//Vector3 rotVec;
+
+	private HingeJoint hingeJoint;
+	private JointMotor moter;
+	// Use this for initialization
+	void Start () {
+	//	speed = 10.0f;
+	//	rotVec = Vector3.zero;
+
+		hingeJoint = GetComponent<HingeJoint> ();
+		moter = new JointMotor ();
+		moter.force = 100;
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		//rotVec += Vector3.up * speed * Time.deltaTime;
+		//transform.rotation = Quaternion.Euler(rotVec);
+
+	//}
+
+	void FixedUpdate(){
+		if(Input.GetKey(KeyCode.Z)){
+			moter.targetVelocity = 360.0f;
+			hingeJoint.motor = moter;
+		} else{
+			moter.targetVelocity = -360.0f;
+			hingeJoint.motor = moter;
+		}
+	}
+}
