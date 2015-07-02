0c28cf4b0447aa2208800a7452d594b2547dbcda
salvere0 <apporiate@naver.com>
2015-07-02 15:58:25 +0900

    20150702
    코인도져

---
+++ 20150702/Assets/Script/CoinGen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinGen : MonoBehaviour {
+	public int autoCoins;
+
+	//private int coinCount;
+	//private float elapseTime;
+
+	// Use this for initialization
+	void Start () {
+		//coinCount = 0;
+		this.StartCoroutine ("AutoGeneration");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && autoCoins > 0) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitObject;
+			Physics.Raycast(ray, out hitObject);
+			GameObject coin = Resources.Load("Coin") as GameObject;
+			Instantiate(coin,hitObject.point, Quaternion.identity);
+		}
+	}
+
+	IEnumerator AutoGeneration(){
+		GameObject coin = Resources.Load("Coin") as GameObject;
+		for (int i = 0; i < autoCoins; ++i)
+		{
+			Instantiate(coin, new Vector3(-0.5f,2f,-3f),Random.rotation);
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
---
+++ 20150702/Assets/Script/Move.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	private Transform testMove;
+
+	private bool direction = true;
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		testMove = GetComponent<Transform> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction)
+		{
+					testMove.Translate (new Vector3 (0.0f, 0.0f, -1.0f) * (speed * Time.deltaTime));
+					if (testMove.position.z <= -2.3f) 
+						{
+								direction = false;
+						}
+		}
+		else
+		{
+			testMove.Translate (new Vector3 (0.0f, 0.0f, 1.0f) * (speed * Time.deltaTime));
+			if (testMove.position.z >= -1.6f)
+			{
+				direction = true;
+			}
+		}
+	
+	}
+}
