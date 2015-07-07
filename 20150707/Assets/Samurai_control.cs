b182cb742305c974aff3865dcc2258a05b0494e7
salvere0 <apporiate@naver.com>
2015-07-07 16:25:30 +0900

    20150707
    사무라이에게 유니티쨩의 애니메이션을 붙임

---
+++ 20150707/Assets/Samurai_control.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Samurai_control : MonoBehaviour {
+
+	private Animator samiAnim;
+
+	// Use this for initialization
+	void Start () {
+		samiAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") != 0) 
+		{
+			samiAnim.SetBool ("Walk", true);
+		} 
+		else 
+		{
+			samiAnim.SetBool("Walk", false);
+		}
+	}
+}
---
+++ 20150707/Assets/Unity_chan_control.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unity_chan_control : MonoBehaviour {
+
+	private Animator unityAnim;
+	// Use this for initialization
+	void Start () {
+		unityAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		unityAnim.SetBool ("Next", true);
+	
+	}
+}
