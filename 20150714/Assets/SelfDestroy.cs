c68fcda535d31920f1f5e6c5595d3a5fb6a91728
salvere0 <apporiate@naver.com>
2015-07-14 16:29:57 +0900

    20150714
    여러가지 처리

---
+++ 20150714/Assets/SelfDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour {
+	[SerializeField]private float Time;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("DestroyThis");
+	}
+	
+	IEnumerator DestroyThis()
+	{
+		yield return new WaitForSeconds(Time);
+		Destroy (this.gameObject);
+	}
+}
---
+++ 20150714/Assets/Sword_collider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword_collider : MonoBehaviour {
+
+	public Transform hitPoint;
+	private Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GameObject.FindWithTag("Player").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Spider") && playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) 
+		{
+			Debug.Log ("아야");
+			GameObject effect = Resources.Load("Spark") as GameObject;
+			Instantiate(effect,
+			            hitPoint.position,
+			            Quaternion.identity);
+		}
+	}
+}
