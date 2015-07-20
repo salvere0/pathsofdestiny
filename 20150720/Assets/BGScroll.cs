173bbadc5cb5569584ae97fe8af4ee7cbb5ca7e0
salvere0 <apporiate@naver.com>
2015-07-20 16:29:01 +0900

    20150720
    2D 배경애니메이션

---
+++ 20150720/Assets/BGScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroll : MonoBehaviour {
+	Transform[] backGrounds;
+	[SerializeField]private float speed = 3f;
+	// Use this for initialization
+	void Start () {
+		backGrounds = GameObject.FindWithTag ("BackGround").GetComponents<Transform> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < backGrounds.Length; ++i) {
+			backGrounds[i].Translate(Vector3.right * speed * Time.deltaTime);
+		}
+	}
+}
